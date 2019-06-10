039082fa078fdc7394f9cf4c7c937cc42efc255e
Kamchan <1031129853@qq.com>
2019-06-10 23:00:10 +0800

    Add dist

---
+++ dist/precache-manifest.eb832cda5bb123eb3969408934f89a2e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "86d51b2c4c56061aa0cd",
+    "url": "/css/app.139f42f7.css"
+  },
+  {
+    "revision": "86d51b2c4c56061aa0cd",
+    "url": "/js/app.df0fbab3.js"
+  },
+  {
+    "revision": "e99bc01b794323ec4083",
+    "url": "/css/chunk-vendors.2c2a0273.css"
+  },
+  {
+    "revision": "e99bc01b794323ec4083",
+    "url": "/js/chunk-vendors.293f81bf.js"
+  },
+  {
+    "revision": "3dcf8d558c2a71f1b889110a5095005d",
+    "url": "/img/logo.3dcf8d55.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d4c20437370894dc395c38d4a6149b65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dcf8d558c2a71f1b889110a5095005d",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
